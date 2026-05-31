b3867c7c22bfeec2788bbcc6264567ae5f01bf6e
Eric Cano <ericcanogimenez@gmail.com>
2026-05-31 15:28:23 +0200

    Refactor Partida class constructor and string method

--- models/partida.py
+++ models/partida.py
@@ -1,27 +1,11 @@
 from datetime import datetime
-from database.database import db_guardar_partida
 
 class Partida:
-    def __init__(self, id_questionari, tipus, id_partida=None, data_partida=None):
+    def __init__(self, id_partida, id_questionari, tipus, data_partida=None):
         self.id_partida = id_partida
-        self.id_questionari = int(id_questionari)
-        
-        if tipus.upper() not in ['INDIVIDUAL', 'VS']:
-            raise ValueError("El tipus de partida ha de ser 'INDIVIDUAL' o 'VS'.")
-        self.tipus = tipus.upper()
-        
-        self.data_partida = data_partida if data_partida else datetime.now()
-
-    def guardar(self, connexio):
-        id_generat = db_guardar_partida(
-            connexio, 
-            self.id_questionari, 
-            self.tipus, 
-            self.data_partida
-        )
-        if id_generat:
-            self.id_partida = id_generat
-        return id_generat
+        self.id_questionari = id_questionari
+        self.tipus = tipus
+        self.data_partida = data_partida or datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     def __str__(self):
-        return f"[PARTIDA {self.id_partida}] Qüestionari: {self.id_questionari} | Tipus: {self.tipus} | Data: {self.data_partida}"
+        return f"Partida {self.id_partida} - Qüestionari {self.id_questionari} - {self.tipus} - {self.data_partida}"
